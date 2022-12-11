834e394a47b9fe6137eeaec96d8912b531bf3cde
CybSec-LAB <102261126+cybsec-lab@users.noreply.github.com>
2022-12-11 19:20:50 +0100

    Create README.md

--- scam_and_phishing_url_scanner.py
+++ scam_and_phishing_url_scanner.py
@@ -12,17 +12,56 @@ Built by integrating the {IPQUALITYSCORE_api}, for ease of access and better ava
 
 
 # required_imports
-from tkinter import *
+#from tkinter import *
 import requests
 import urllib
 import json
 
-# simple_detection_code
+"""ws = Tk()
+ws.title('SCAM SCANNER')
+ws.geometry('400x300')
 
+f = ('Times', 14)
+
+left_frame = Frame(
+    ws,
+    bd=2,
+    bg='#CCCCCC',
+    relief=SOLID,
+    padx=10,
+    pady=10
+    )
+
+Label(
+    left_frame,
+    text="Link",
+    bg='#CCCCCC',
+    font=f).grid(row=0, column=0, sticky=W, pady=10)
+
+Link_tf = Entry(
+    left_frame,
+    font=f
+    ) """
+
+
+
+# simple_detector
 url = input("Enter a url: ")
 encoded_url = urllib.parse.quote(url, safe='')
 api_url = "https://ipqualityscore.com/api/json/url/flO6UAmrHHJk513EmBItfxyrWkM9p2Kz/"
-response = requests.get(api_url + encoded_url)
+#strictness = input("choose 0-1:  ")
+#additional_params = {'strictness': strictness}
+response = requests.get(api_url + encoded_url + """additional_params""")
+
+"""scan_btn = Button(
+    left_frame,
+    width=15,
+    text='Login',
+    font=f,
+    relief=SOLID,
+    cursor='hand2',
+    command=encoded_url
+    )"""
 
 
 # viewing response
@@ -31,3 +70,8 @@ print(json.dumps(data.json(), indent=4))
 
 
 # The user interface
+"""Link_tf.grid(row=0, column=1, pady=10, padx=20)
+scan_btn.grid(row=2, column=1, pady=10, padx=20)
+left_frame.pack()
+
+ws.mainloop()"""
