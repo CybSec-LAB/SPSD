51d7e997dc3c8ecc8cd827625b8a01994d3eef01
CybSec-LAB <cybseclab@hotmail.com>
2022-12-13 03:01:55 -0800

    Create README.md

--- Linux/scam_and_phishing_url_scanner.py
+++ Linux/scam_and_phishing_url_scanner.py
@@ -2,6 +2,7 @@ import json
 import urllib
 import requests
 
+
 # simple_detector
 url = input("Enter a url: ")
 encoded_url = urllib.parse.quote(url, safe='')
@@ -9,6 +10,7 @@ api_url = "https://ipqualityscore.com/api/json/url/flO6UAmrHHJk513EmBItfxyrWkM9p
 #strictness = input("choose 0-1:  ")
 #additional_params = {'strictness': strictness}
 response = requests.get(api_url + encoded_url + """additional_params""")
+
 
 # viewing response
 data = requests.get(api_url + encoded_url)
